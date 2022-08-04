a9e53662de41b7f18c814bbeed8633cccc3e3219
Matt Harten <108069320+MGHarten@users.noreply.github.com>
2022-08-03 17:29:36 -0700

    Scuba Fix #2
    Added px to width and height values

---
+++ app.py
@@ -0,0 +1,31 @@
+<div class = "container" id = "Contact" >
+        <div style = "text-align:center" >
+          <h2 > Contact Me < /h2 >
+          <p > Have a project idea or a job opportunity? Let me know: < /p >
+        </div >
+        <div class = "row" >
+          <div class = "column" >
+            <img src = "assets/img/scuba.jpg" width = "427px" height = "640px" alt = "Photo of me scuba diving taken by Ashley White" >
+          </div >
+          <div class = "column" >
+            <form action = "/action_page.php" >
+              <label for = "fname" > First Name < /label >
+              <input type = "text" id = "fname" name = "firstname" placeholder = "Your name.." >
+              <label for = "lname" > Last Name < /label >
+              <input type = "text" id = "lname" name = "lastname" placeholder = "Your last name.." >
+              <label for = "country" > Region < /label >
+              <select id = "country" name = "country" >
+                <option value = "usa" > Africa < /option >
+                <option value = "usa" > Asia < /option >
+                <option value = "australia" > Australia < /option >
+                <option value = "usa" > Europe < /option >
+                <option value = "usa" > Middle East < /option >
+                <option value = "canada" > North America < /option >
+              </select >
+              <label for = "subject" > Subject < /label >
+              <textarea id = "subject" name = "subject" placeholder = "Write something.." style = "height:170px" > </textarea >
+              <input type = "submit" value = "Submit" >
+            </form >
+          </div >
+        </div >
+      </div>
